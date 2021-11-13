b1bde0e2e453fcb797347248dbc05004dcf1da61
Jack Trimboli <67242835+JackTrimboli@users.noreply.github.com>
2021-11-13 17:39:03 -0500

    animals change

--- server/routes/animals.js
+++ server/routes/animals.js
@@ -1,13 +1,12 @@
 const express = require("express");
 
 const petfinder = require('pet-finder-api')('api_key', 'api_secret');
-
 const petFinderRoutes = express.Router();
 
 petFinderRoutes.get('/search', (req, res) => {
     res.send(petfinder.findPet('dog', (x) => {
         console.log(x);
     }))
-})
+});
 
 module.exports = petFinderRoutes;
