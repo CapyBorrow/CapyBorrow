da775e06111dbdec3a2d562a0a702c44c480c8b3
Mark Cruz <jink144@gmail.com>
2021-11-13 14:55:46 -0500

    init of navbar

--- client/src/Components/NavBar/NavBar.js
+++ client/src/Components/NavBar/NavBar.js
@@ -1,16 +1,33 @@
+import {makeStyles,} from '@material-ui/core';
 import React from 'react';
-import Drawer from '@mui/material/Drawer';
+import Drawer from '@material-ui/core/Drawer';
+import Typography from '@material-ui/core/Typography';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
 
 import './NavBar.css';
+import useStyles from './styles.js';
 
-class NavBar extends React.Component {
-    render() {
-        return (
-            <Drawer positon="static">
-                test
-            </Drawer>
-        );
-    }
+const NavBar = ({}) => {
+	const classes = useStyles(); // Adding a classname={classes.<componentName styles in styles.js}
+	{
+		return (
+			<Drawer className={classes.drawer} variant='permanent' anchor='left'>
+				<div>
+					<ListItemButton component='a' href='#simple-list'>
+						<ListItemText primary='Home' />
+					</ListItemButton>
+                    <ListItemButton component='a' href='#simple-list'>
+						<ListItemText primary='Vets' />
+					</ListItemButton>
+                    <ListItemButton component='a' href='#simple-list'>
+						<ListItemText primary='Shelters' />
+					</ListItemButton>
+				</div>
+			</Drawer>
+		);
+	}
 };
 
 export default NavBar;
---
+++ client/src/Components/NavBar/styles.js
@@ -0,0 +1,9 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const drawerWidth = 2000;
+
+export default makeStyles((theme) => ({
+    drawer: {
+        width: drawerWidth
+    }
+}));
