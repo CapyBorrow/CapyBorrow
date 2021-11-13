1086855883b74611d5e5e67f1d48f3a4f4841a36
Mark Cruz <jink144@gmail.com>
2021-11-13 16:39:44 -0500

    fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
 import NavBar from './Components/NavBar/NavBar';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+import logo from '../src/images/logo2.png';
 
 const App = () => {
-  return (
-    <div className="App">
-      <NavBar />
-    </div>
-  );
-}
+	return (
+		<div className='App'>
+			<AppBar position='static'>
+				<Toolbar variant='dense'>
+          <img src={logo}/>
+					<IconButton edge='start' color='inherit' aria-label='menu' sx={{mr: 2}}>
+						<MenuIcon />
+					</IconButton>
+				</Toolbar>
+			</AppBar>
+		</div>
+	);
+};
 
 export default App;
--- client/src/Components/NavBar/NavBar.js
+++ client/src/Components/NavBar/NavBar.js
@@ -1,10 +1,11 @@
-import {makeStyles,} from '@material-ui/core';
+import {makeStyles} from '@material-ui/core';
 import React from 'react';
 import Drawer from '@material-ui/core/Drawer';
 import Typography from '@material-ui/core/Typography';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
+import ImageListItem from '@mui/material/ImageListItem';
 
 import './NavBar.css';
 import useStyles from './styles.js';
@@ -16,13 +17,10 @@ const NavBar = ({}) => {
 			<Drawer className={classes.drawer} variant='permanent' anchor='left'>
 				<div>
 					<ListItemButton component='a' href='#simple-list'>
-						<ListItemText primary='Home' />
+						<ListItemText primary='Menu' />
 					</ListItemButton>
-                    <ListItemButton component='a' href='#simple-list'>
-						<ListItemText primary='Vets' />
-					</ListItemButton>
-                    <ListItemButton component='a' href='#simple-list'>
-						<ListItemText primary='Shelters' />
+					<ListItemButton component='a' href='#simple-list'>
+						<ListItemText primary='Search Pets' />
 					</ListItemButton>
 				</div>
 			</Drawer>
