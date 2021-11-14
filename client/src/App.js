2d1c5b6750a5746715620237081f983264f226de
Mark Cruz <jink144@gmail.com>
2021-11-13 22:28:04 -0500

    revert not fucking around

--- client/src/App.js
+++ client/src/App.js
@@ -24,15 +24,6 @@ const App = () => {
 			<div className='App'>
 				<NavBar />
 				<SideNav />
-					<Grow in>
-						<Container>
-							<Grid container justify='space-between' alignItems='stretch' spacing={3}>
-								<Grid item xs={12} sm={7}>
-									<PetCards />
-								</Grid>
-							</Grid>
-						</Container>
-					</Grow>
 			</div>
 		);
 	}
