151c1385a76ac4d3979a180b8403210e9412e23b
JustANormalDuck <65052456+JustANormalDuck@users.noreply.github.com>
2021-11-14 03:23:24 -0500

    Updated animals.js

--- server/routes/animals.js
+++ server/routes/animals.js
@@ -1,18 +1,32 @@
 const express = require("express");
 const axios = require('axios').default
 const petFinderRoutes = express.Router();
-petFinderRoutes.get('/search', (req, res) => {
-  var arr = []
-  axios.get(`https://www.petfinder.com/api/v2/animals?type=${petType}&page=${pageNum}`,
-    {
-      headers: { Authorization: `Bearer ${petFinderToken}` }
-    }).then(
-      function (response) {
-        // handle success
-        for (var key in response.data.animals) {
-          arr.push(response.data.animals[key])
-        }
-        res.send(arr)
-      })
+petFinderRoutes.get('/search',(req, res) => {
+    let petName = req.query.petName
+    let pageNum = req.query.pageNum
+    axios.post('https://api.petfinder.com/v2/oauth2/token', {
+  grant_type:"client_credentials",
+  client_id:"VqZLSPakyBxQLZZgwUZ1W0WRB6EPXYlt1EoMqCneAYUxQxGXTZ",
+  client_secret:"FhUD4JVrS9Wxyt1ICWKBojU2iqu4wilBRsWz6JBY",
 })
-module.exports = petFinderRoutes;
+.then(function (response) {
+  var arr=[]
+  petFinderToken = response.data["access_token"];
+  axios.get(`https://www.petfinder.com/api/v2/animals?type=${petName}&page=${pageNum}`, 
+         {
+            headers : {Authorization:`Bearer ${petFinderToken}`}
+         }).then(
+             function (response)
+            {
+            // handle success
+            for(var key in response.data.animals)
+            {
+              arr.push(response.data.animals[key])
+            }
+            console.log(arr)
+            })
+})
+.catch(function (error) {
+  res.send(arr)
+});
+})
